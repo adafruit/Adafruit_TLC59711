16c8451b73a36a755b6ad3746a854effc7b35402
Doxygen CI <ci-arduino@invalid>
2021-06-05 03:02:11 +0000

    Deploy docs to GitHub Pages from commit bc84808370
    Commit: bc84808370674dad6611ee114cfb967af882ce3b
    GitHub Actions run: 908275335

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -3,5 +3,7 @@ var searchData=
   ['setbrightness',['setBrightness',['../class_adafruit___t_l_c59711.html#a28ef6f75e940e3f8cba4b728dadb63aa',1,'Adafruit_TLC59711']]],
   ['setled',['setLED',['../class_adafruit___t_l_c59711.html#a8a2018b1ef1141ccb637cf02fb059035',1,'Adafruit_TLC59711']]],
   ['setpwm',['setPWM',['../class_adafruit___t_l_c59711.html#a39bfa456b8b08c9f026144165534a4a8',1,'Adafruit_TLC59711']]],
-  ['simplesetbrightness',['simpleSetBrightness',['../class_adafruit___t_l_c59711.html#a4dfbe59e34f01ead81a086f2626ec380',1,'Adafruit_TLC59711']]]
+  ['simplesetbrightness',['simpleSetBrightness',['../class_adafruit___t_l_c59711.html#a4dfbe59e34f01ead81a086f2626ec380',1,'Adafruit_TLC59711']]],
+  ['spi_5fsettings',['SPI_SETTINGS',['../_adafruit___t_l_c59711_8cpp.html#aef0d85948425dfcdae1adeed83d6a1a4',1,'Adafruit_TLC59711.cpp']]],
+  ['spiwritemsb',['spiwriteMSB',['../class_adafruit___t_l_c59711.html#ab0d1c38dfc60efd3552c50a51348018c',1,'Adafruit_TLC59711']]]
 ];

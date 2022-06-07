b406ded61b84c17ab4ae94db83ec2a7aa36f17e9
Doxygen CI <ci-arduino@invalid>
2022-06-07 15:38:39 +0000

    Deploy docs to GitHub Pages from commit 367ec77e03
    Commit: 367ec77e03935dbef31193a726c72b741def7c0c
    GitHub Actions run: 2455693059

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['setbrightness',['setBrightness',['../class_adafruit___t_l_c59711.html#a28ef6f75e940e3f8cba4b728dadb63aa',1,'Adafruit_TLC59711']]],
   ['setled',['setLED',['../class_adafruit___t_l_c59711.html#a8a2018b1ef1141ccb637cf02fb059035',1,'Adafruit_TLC59711']]],
-  ['setpwm',['setPWM',['../class_adafruit___t_l_c59711.html#a39bfa456b8b08c9f026144165534a4a8',1,'Adafruit_TLC59711']]],
+  ['setpwm',['setPWM',['../class_adafruit___t_l_c59711.html#afa389df3b9163545fd82c5d43f244497',1,'Adafruit_TLC59711']]],
   ['simplesetbrightness',['simpleSetBrightness',['../class_adafruit___t_l_c59711.html#a4dfbe59e34f01ead81a086f2626ec380',1,'Adafruit_TLC59711']]]
 ];
